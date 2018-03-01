fadc8c363c4b358f10312cc7211afc22db7f2236
elssbbboy <elssbbboy@gmail.com>
2018-03-01 20:10:06 +0100

    Order function and distance calc finished, schedule function in progress

--- scheduling.py
+++ scheduling.py
@@ -21,24 +21,41 @@ def ride_distance(ride):
 	id
 '''
 
-def sort_rides(rides, **kwargs): # kwargs['mode']: 0 -> ordenar por start, 1 -> ordenar por ride_distance
 
+
+
+def sort_rides(rides, **kwargs): # kwargs['mode']: 0 -> ordenar por start, 1 -> ordenar por ride_distance
+# 2 -> finish
 	'''
 	# En caso de emergencias
 	if(kwargs):
 			l_ord = []
-			if(kwargs[0]=='0'):
+			if(kwargs['mode']=='0'):
 				for i in rides:
 					l_ord.append((i[5],i[2]))
 					l_ord.sort(key=lambda x: x[1])
 					return [i[0] for i in l_ord]
-			else:
+
+			elif (kwargs['mode']=='1'):
 				for i in rides:
-					l_ord.append((i[5],ride_distance(i[2],i[3])))
+					l_ord.append((i[5],ride_distance(i[0],i[1])))
 					l_ord.sort(key=lambda x: x[1])
 					return [i[0] for i in l_ord]
+			else:
+				for i in rides:
+					l_ord.append((i[5],i[3])
+					l_ord.sort(key=lambda x: x[1])
+					return [i[0] for i in l_ord]
+
 	'''
-	return sorted(rides, key = ride_distance)
+    if(kwargs['mode']=='0'):
+        k = lambda ride: ride[2]
+    elif (kwargs['mode']=='1'):
+		k = ride_distance
+	else:
+        k = lambda ride: ride[3]
+
+	return sorted(rides, key = k)
 
 def main(argv): # We expect to receive input file as first argument and output file second argument (optional). If output not specified, defaults to input+.out
 	if len(argv) < 1:
@@ -90,10 +107,21 @@ map:
 '''
 def schedule(map_info, rides):
     step = 0
-    sort_rides(rides, mode=0)
-    vehicles = [[i, None] for i in range(map_info[2])] # Lista de vehiculos con su id y su objetivo
+    orides = sort_rides(rides, mode=2)
+    freev = [[i, (0, 0), None] for i in range(map_info[2])] # Lista de vehiculos con su id, posicion y su ride asignado
+    workv = [] # Lista de vehiculos trabajando
+    donejob = {} # Diccionario que indiza por id de vehiculo y que almacena listas de trabajos completados
     mapsim = np.zeros((map_info[0], map_info[1]))
     mapsim[0,0] = map_info[2]
+    currentrides = []
+
     while true:
-        for veh in vehicles:
-            if veh[1] is None:
+        ridestodel = []
+        for i in range(len(orides)):
+            if len(freev) > 0:
+                freev.sort(key = lambda v: man_distance(v[1], orides[i][0]))
+                endtime = man_distance(freev[0][1], orides[i][0]) +
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
